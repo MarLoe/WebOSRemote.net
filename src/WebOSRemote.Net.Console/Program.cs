49feb2c11f152a1d02cae419f1558ad45d141749
Martin Lobger <martin@lobger.com>
2023-01-26 23:37:37 +0100

    Adding sample program

---
+++ src/WebOSRemote.Net.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using WebOsRemote.Net;
+using WebOsRemote.Net.Device;
+
+namespace WebOSRemote.Net.Console
+{
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var device = new WebOSDevice() { HostName = "LGwebOSTV.local" };
+            var device = new WebOSDevice() { IPAddress = "192.168.100.116" };
+            using var client = new WebOSClient();
+            await client.Attach(device);
+            await client.ConnectAsync();
+
+        }
+    }
+}
+
